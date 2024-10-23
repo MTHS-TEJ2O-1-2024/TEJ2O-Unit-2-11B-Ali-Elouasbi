f8c962fc6115028daed880576b99ae01a2ba0f43
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-10-23 15:52:34 -0400

    changed code

--- main.ts
+++ main.ts
@@ -5,19 +5,33 @@
  * This program ...
 */
 
+//setup
+basic.showIcon(IconNames.Happy)
+
+//variables
 let firstNumber = randint(0, 99)
 let secondNumber = randint(0, 99)
 
+//Button A
 input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
     basic.showNumber(firstNumber)
-})
-input.onButtonPressed(Button.B, function() {
-    basic.showNumber(secondNumber)
+    basic.showIcon(IconNames.Happy)
 })
 
+//Button B
+input.onButtonPressed(Button.B, function() {
+    basic.clearScreen()
+    basic.showNumber(secondNumber)
+    basic.showIcon(IconNames.Happy)
+})
+
+//Shake
 input.onGesture(Gesture.Shake, function() {
+   basic.clearScreen()
     if (firstNumber < secondNumber) 
         basic.showString(`${firstNumber} < ${secondNumber}`)
     else if (firstNumber > secondNumber)
         basic.showString(`${firstNumber} > ${secondNumber}`)
+        basic.showIcon(IconNames.Sad)
 })
